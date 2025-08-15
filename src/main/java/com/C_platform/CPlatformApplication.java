17d483061a1515dc389950b37daba080149614ef
WoonKang <e2505011@gmail.com>
2025-08-15 20:40:32 +0900

    setting

---
+++ src/main/java/com/C_platform/CPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.C_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/C_platform/CPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.C_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
