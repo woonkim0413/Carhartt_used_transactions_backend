ed4d1b82daf321e3bc818f2d431cfe28e5b5f667
WoonKang <e2505011@gmail.com>
2025-08-17 13:58:31 +0900

    [API 변경] test

--- src/main/java/com/C_platform/CPlatformApplication.java
+++ src/main/java/com/C_platform/CPlatformApplication.java
@@ -3,11 +3,11 @@ package com.C_platform;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// test 중
 @SpringBootApplication
 public class CPlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CPlatformApplication.class, args);
 	}
-
 }
