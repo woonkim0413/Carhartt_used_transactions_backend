854a8b18b46c35af44c6ec2de6f689a5f594a1ad
WoonKang <e2505011@gmail.com>
2025-09-12 18:43:50 +0900

    Entity create

---
+++ src/main/java/com/C_platform/Member/domain/Member.java
@@ -0,0 +1,64 @@
+package com.C_platform.Member.domain;
+
+import com.C_platform.order.Order;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "member")
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Column(name = "member_name")
+    private String name;
+
+    @Column(name = "email") // local일 때 사용
+    private String email;
+
+    @Column(name = "login_password") // local일 때 사용
+    private String loginPassword;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "oauth_provider")
+    private OAuthProvider oauthProvider;
+
+    @Column(name = "oauth_id")
+    private String oauthId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "login_type")
+    private loginType loginType;
+
+    @JoinColumn(name = "address_id")
+    private int defaultAddressId;
+
+    @Column(name = "image_url")
+    private String profileImageUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "count_bank")
+    private BankName accountBank;
+
+    @Column(name = "count_number") // = account number
+    private String accountNumber;
+
+    // ---------- 연관관계 객체들 -----------------------
+    // Address와 연관관계로 묶인 field
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
+
+    // Order와 연관관계로 묶인 field
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+    // Wish와 연관관계로 묶인 field
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Wish> wishes = new ArrayList<>();
+}
--- src/main/java/com/C_platform/Member/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.C_platform.Member.repository;
-
-import com.C_platform.Member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Integer> {
-}
