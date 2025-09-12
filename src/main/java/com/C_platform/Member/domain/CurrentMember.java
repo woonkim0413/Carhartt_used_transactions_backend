7a4a2380fce1fde479bce3d6b92b4d9014d24c05
WoonKang <e2505011@gmail.com>
2025-09-12 20:57:51 +0900

    create getMemberId(anywhere)

--- src/main/java/com/C_platform/Member/domain/CurrentMember.java
+++ src/main/java/com/C_platform/Member/domain/CurrentMember.java
@@ -1,10 +1,13 @@
 package com.C_platform.Member.domain;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 // 현재 로그인 중인 member_id를 주문이나 상품 domain에서 얻을 수 있도록 만들었습니다
+@Component
 public class CurrentMember {
-    public Long idOrThrow() {
+    // parameter 없이 어디서든 현재 로그인 된 회원 id 반환
+    public Long currnetMemberId() {
         var auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth == null || !auth.isAuthenticated())
             throw new IllegalStateException("미인증");
