362881f43612706d0830de86d437988676360f3d
WoonKang <e2505011@gmail.com>
2025-09-12 21:49:19 +0900

    complete getMemberId

--- src/main/java/com/C_platform/global/MetaData.java
+++ src/main/java/com/C_platform/global/MetaData.java
@@ -9,7 +9,6 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class MetaData {
-
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     private LocalDateTime timestamp;
 }
