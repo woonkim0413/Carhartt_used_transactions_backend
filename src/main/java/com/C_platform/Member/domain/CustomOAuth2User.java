bc5cc270b603c17775c0d5bc8680020fe9b7fb7a
WoonKang <e2505011@gmail.com>
2025-09-12 20:57:51 +0900

    create getMemberId(anywhere)

--- src/main/java/com/C_platform/Member/domain/CustomOAuth2User.java
+++ src/main/java/com/C_platform/Member/domain/CustomOAuth2User.java
@@ -6,6 +6,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import java.util.Collection;
 import java.util.Map;
 
+// Spring Security가 인증을 끝낸 뒤 SecurityContext에 넣어두는 사용자 대표 객체이다
 public class CustomOAuth2User implements OAuth2User {
     // principal로 사용할 객체
     private final Long memberId;
@@ -20,6 +21,7 @@ public class CustomOAuth2User implements OAuth2User {
         this.authorities = authorities;
     }
 
+    // CurrentMember에 사용됨
     public Long getMemberId() {
         return memberId;
     }
