45e5863279b96246f47e9e6401dc1d0b537679f1
Tarte <emprimula@gmail.com>
2025-11-10 17:06:59 +0900

    fix: url

--- src/main/java/com/C_platform/item/infrastructure/ItemRepository.java
+++ src/main/java/com/C_platform/item/infrastructure/ItemRepository.java
@@ -7,9 +7,12 @@ import com.C_platform.item.domain.ItemStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item,Long> {
@@ -17,4 +20,7 @@ public interface ItemRepository extends JpaRepository<Item,Long> {
     List<Item> findByMemberAndStatusIn(Member member, List<ItemStatus> statuses);
 
     Page<Item> findByNameLike(String name, Pageable pageable);
+
+    @Query("SELECT i FROM Item i LEFT JOIN FETCH i.images WHERE i.id = :itemId")
+    Optional<Item> findByIdWithImages(@Param("itemId") Long itemId);
 }
