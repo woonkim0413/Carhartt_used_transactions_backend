179584fbf1daa8d0a36749205719a501e65e4476
Tarte <emprimula@gmail.com>
2025-11-11 20:23:21 +0900

    fix: ItemView

--- src/main/java/com/C_platform/order/application/port/ItemPricingReader.java
+++ src/main/java/com/C_platform/order/application/port/ItemPricingReader.java
@@ -1,9 +1,11 @@
 package com.C_platform.order.application.port;
 
+import com.C_platform.item.domain.ItemStatus;
+
 public interface ItemPricingReader {
 
     ItemView getById(Long itemId);
 
     // 주문 생성에 필요한 최소 정보만
-    record ItemView(Long id, String name, Long sellerId, Integer price, String status) {}
+    record ItemView(Long id, String name, Long sellerId, Integer price, ItemStatus status) {}
 }
